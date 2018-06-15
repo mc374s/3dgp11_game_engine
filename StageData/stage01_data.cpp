8c1c734f6fb7d979e04b1bd02a96156883850b38
岬 前田 <misaki.game.0811@gmail.com>
2018-06-15 16:41:13 +0900

    stage_data変更 ジャンプエフェクト画像差し替え

--- StageData/stage01_data.cpp
+++ StageData/stage01_data.cpp
@@ -12,7 +12,7 @@ STAGE_DATA stage01_setData[] = {
 
 	//ページ２
 	STAGE_DATA(e_startPagination[1] + 1, 0, M_TYPE::BORDER_INNER,			M_DRAW::UP,		v3(0,PH / 2,1),		true, v3(PW ,11,0), 10),
-	STAGE_DATA(e_startPagination[1] + 1, 0, M_TYPE::RECOVERY,				M_DRAW::UP,		v3(100,PH / 2 - 64,0),	true, v3(64,64,0), 2),
+	//STAGE_DATA(e_startPagination[1] + 1, 0, M_TYPE::RECOVERY,				M_DRAW::UP,		v3(100,PH / 2 - 64,0),	true, v3(64,64,0), 2),
 	STAGE_DATA(e_startPagination[1] + 1, 0, M_TYPE::DOOR,					M_DRAW::UP,		v3(450,PH / 2 - 76,0),	true, v3(70,76,0), LOW_CONCENTRATION),
 
 
