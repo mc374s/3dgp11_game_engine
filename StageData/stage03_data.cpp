45561de394d1e8ff35140870e6c3282cfcb200e9
o-ni- <m.miu0024@gmail.com>
2018-06-20 23:59:00 +0900

    背景データ変更

--- StageData/stage03_data.cpp
+++ StageData/stage03_data.cpp
@@ -21,6 +21,7 @@ STAGE_DATA stage03_setData[] = {
 	STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 3], 0, M_TYPE::TUTORIAL_4,				M_DRAW::UP,		v3(300,120,0),	false, v3(239,55,0), 10),
 
 	STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 3], 0, M_TYPE::BIRD_8,				M_DRAW::UP,		v3(80,220 - 148,0),	false, v3(192,148,0), 10),
+	STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 3], 0, M_TYPE::BIRD_3,				M_DRAW::MIRROR,		v3(420,460 - 91,0),	false, v3(190,91,0), 10),
 	STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 3], 0, M_TYPE::BIRD_4,				M_DRAW::UP,		v3(180,680 - 85,0),	false, v3(58,85,0), 10),
 	STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 3], 0, M_TYPE::BIRD_5,				M_DRAW::UP,		v3(380,680 - 85,0),	false, v3(57,85,0), 10),
 
@@ -35,7 +36,10 @@ STAGE_DATA stage03_setData[] = {
 	STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 3] + 1, 0, M_TYPE::BORDER_INNER,				M_DRAW::LEFT,	v3(PW / 2,231,1),		true, v3(11,449,0), 10),
 
 	STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 3] + 1,	 0,	M_TYPE::DOOR,						M_DRAW::UP,		v3(100,680 - 111,0),	true, v3(124,111,0), 8),
-	STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 3] + 1, 0, M_TYPE::BIRD_7,				M_DRAW::UP,		v3(400,680 - 165,0),	false, v3(158,165,0), 10),
+
+	STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 3] + 1, 0, M_TYPE::BIRD_2,				M_DRAW::UP,		v3(300,220 - 137,0),	false, v3(138,137,0), 10),
+	STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 3] + 1, 0, M_TYPE::BIRD_3,				M_DRAW::UP,		v3(420,220 - 91,0),	false, v3(190,91,0), 10),
+	STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 3] + 1, 0, M_TYPE::BIRD_7,				M_DRAW::UP,		v3(340,680 - 165,0),	false, v3(158,165,0), 10),
 
 
 	// END
