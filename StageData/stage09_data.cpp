1bb2dad5616985475bc6d9332324b64893899611
mc374s <chenyuezong-codegeass@live.com>
2018-06-25 03:31:21 +0900

    色調整

--- StageData/stage09_data.cpp
+++ StageData/stage09_data.cpp
@@ -69,7 +69,7 @@ STAGE_DATA stage09_setData[] = {
 
 	STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 4 + 9] + 1,0, M_TYPE::RECOVERY,				M_DRAW::UP,		v3(500,1400 - 64,0),	true, v3(71,64,0), 2),
 
-
+	 
 
 	//STAGE_DATA(e_startPagination[STAGE_SELECT_MAX_NUM + 2] + 1, 0, M_TYPE::KEY_1_1,						M_DRAW::UP,		v3(120,900 - 30,0),	true, v3(50,30,0), 10),
 
